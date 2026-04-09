4ade7311f21c14716f08225f0e336d08c65164d2
drrible <asdasdarqw3123123q05@mail.ru>
2026-04-09 14:26:15 +0300

    New Commit

---
+++ resources/views/content.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE=<html>
+<head><body>
+<meta charset="UTF-8">
+
+
+<title>Content</title>
+
+
+
+
+<h ref="{{ route_ ('app\Http\Controllers\ContentController@index') }}">Content</h>
+
+Laravel v {{Illuminate\Foundation\Application:: ID  {{ session()->getId() }}
+
+
+
+</head></body></html>
