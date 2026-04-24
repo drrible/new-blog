01426b54ed854758957eae525ed7881a01c39224
drrible <asdasdarqw3123123q05@mail.ru>
2026-04-24 16:07:48 +0300

    new Commit "Deleting migrations"

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,7 +1,10 @@
 @include ('section')
 <php __env->startSection('message , __ ('Start_error')) ?>
 
-<!-- comment -->
+
+
+
+To check the repair status, please enter your repair ticket or serial number. If you are trying to search the site for a product model or some support content, click here to complete that search.
 @endsection
 
 
