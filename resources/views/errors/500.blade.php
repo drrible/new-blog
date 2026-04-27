32097f969803e1013f851c590bfdd7fa87787d8c
drrible <asdasdarqw3123123q05@mail.ru>
2026-04-27 10:52:15 +0300

    new Commit:  "updated list of: ' 500.blade.php, app.css, index.html'"

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -4,7 +4,6 @@
 
 
 
-To check the repair status, please enter your repair ticket or serial number. If you are trying to search the site for a product model or some support content, click here to complete that search.
 @endsection
 
 
